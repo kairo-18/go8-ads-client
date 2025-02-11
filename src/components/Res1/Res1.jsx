ddca9158fc061781b832c258e88dc47965f1ad9b
kairo-18 <gladimirdungo@gmail.com>
2025-02-11 11:04:55 +0800

    added gif

--- src/components/Res1/Res1.jsx
+++ src/components/Res1/Res1.jsx
@@ -1,25 +1,47 @@
-import React, {useState, useEffect} from 'react'
-import FlightBoard from './FlightBoard';
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import FlightBoard from "./FlightBoard";
+import sampleAds from "./Adobe_ad.gif";
 
 function Res1() {
     const [isAds, toggleAds] = useState(false);
+
     useEffect(() => {
         const interval = setInterval(() => {
-            toggleAds(!isAds);
-        }, 5000);
+            toggleAds((prev) => !prev);
+        }, 15000);
         return () => clearInterval(interval);
-    }, [isAds]);
-return (
-    <div className='w-screen h-screen flex'>
-        {/*This div is the flight detail board*/ }
-        <div className={isAds ? 'w-3/4' : 'w-full'}>
-            <FlightBoard />
+    }, []);
+
+    return (
+        <div className="w-screen h-screen flex overflow-hidden">
+            {/* Flight detail board */}
+            <div
+                className={
+                    isAds
+                        ? "w-3/4 transition-all duration-500"
+                        : "w-full transition-all duration-500"
+                }
+            >
+                <FlightBoard />
+            </div>
+
+            {/* Ads div with animation */}
+            <AnimatePresence>
+                {isAds && (
+                    <motion.div
+                        initial={{ x: "100%" }}
+                        animate={{ x: 0 }}
+                        exit={{ x: "100%" }}
+                        transition={{ duration: 0.5, ease: "easeInOut" }}
+                        className="ads w-1/4 bg-black text-white flex items-center justify-center"
+                    >
+                        <img className="w-full h-full" src={sampleAds} alt="ads" />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
-        {/*This div is the ads*/ }
-        
-        {isAds && <div className='ads w-1/6'>Hotdog</div>}
-    </div>
-)
+    );
 }
 
 export default Res1;
