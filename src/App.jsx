f8bcba6cc8f6448ffddbe1468a0d41a375bd2275
LanceP23 <biggyfastboi@gmail.com>
2025-02-13 13:22:51 +0800

    added routes and mui

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Res1 from './components/Res1/Res1';
+import AdminDashboard from './pages/AdminDashboard';
+import AdminCRUD from './pages/AdminCRUD';
+import CreateScreen from './pages/CRUD/CreateScreen';
+import UpdateScreen from './pages/CRUD/UpdateScreen';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +15,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Res1 />} />
+        <Route path="/admin" element={<AdminDashboard/>}/>
+        <Route path="/admin/crud" element={<AdminCRUD/>}/>
+        <Route path="/admin/crud/create" element={<CreateScreen/>}/>
+        <Route path="/admin/crud/update" element={<UpdateScreen/>}/>
       </Routes>
     </Router>
     
---
+++ src/pages/CRUD/UpdateScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import DeleteScreen from './DeleteScreen'
+import Button from '@mui/material/Button';
+
+const UpdateScreen = () => {
+  return (
+    <div>
+      update
+      <div>
+      <Button variant="contained">Hello world</Button>;
+        <DeleteScreen/>
+      </div>
+    </div>
+  )
+}
+
+export default UpdateScreen
