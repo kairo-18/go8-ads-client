367cd988b32a68551b771f2dbc95503b03469a2f
paulluiserdev <pvasquez@qmdsi.com>
2025-02-17 17:15:23 +0800

    Changed Screen Preview and added component AdminPreview

--- src/pages/Admin/AdminPreviews.jsx
+++ src/pages/Admin/AdminPreviews.jsx
@@ -5,14 +5,16 @@ import Res1 from "../../components/Res1/Res1";
 import Res2 from "../../components/Res2/Res2";
 import Res3 from "../../components/Res3/Res3";
 import { useNavigate } from "react-router-dom";
+import DashboardStats from "../../components/Admin/DashboardStats";
 
 function AdminPanel() {
     return (
         <div className="">
             <SideBar />
             <div className="w-full h-screen p-10 bg-gray-50">
-                <div className="ml-64">
-                    <h1 className="font-bold text-2xl">Screen Previews</h1>
+                <div className="ml-64 flex flex-col gap-1">
+                    <h1 className="font-bold text-2xl">Previews</h1>
+                    <DashboardStats />
                     <ScreenPreview />
                 </div>
             </div>
