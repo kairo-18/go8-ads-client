94e48b098d71b10f27611d47492398aa9913d5f1
paulluiser <pvasquez@qmdsi.com>
2025-02-17 13:57:45 +0800

    Sidebar changes and logout btn

--- src/pages/SideBar.jsx
+++ src/pages/SideBar.jsx
@@ -1,38 +1,50 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import logo from "/src/assets/sidebar/logo.png";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
 
 function SideBar() {
     const navigate = useNavigate();
+    const { logout } = useContext(UserContext); 
+
     return (
-        <div className="w-64 h-full p-4 absolute font-[Inter] font-light bg-white">
-            <img src={logo} alt="logo" className="w-32 mx-auto my-15" />
-            <div className="flex flex-col justify-between">
-                <ul>
-                    {["Dashboard", "Previews", "Ad Setting", "Scheduling"].map(
-                        (text) => (
-                            <li key={text} className="p-0">
-                                <button
-                                    className="p-5 hover:bg-gray-200 w-full text-left"
-                                    onClick={() =>
-                                        navigate(
-                                            `/admin/${text
-                                                .toLowerCase()
-                                                .replace(" ", "-")}`
-                                        )
-                                    }
-                                >
-                                    {text}
-                                </button>
-                            </li>
-                        )
-                    )}
-                </ul>
-                <hr className="my-5" />
-                <button className="p-5 hover:bg-gray-200 text-left">
-                    L ogout
-                </button>
+        <div className="w-64 h-full py-4 absolute font-[Inter] font-light bg-[#EEEEEE] flex flex-col justify-between">
+            <a href="/">
+                <img src={logo} alt="logo" className="w-32 mx-auto my-15 hover:shadow-lg rounded-xl hover:border-[#D9D9D9]" />
+            </a>
+            <div className="flex flex-col justify-between h-full ">
+                <div className="">
+                    <ul>
+                        {["Dashboard", "Previews", "Ad Setting", "Scheduling"].map(
+                            (text) => (
+                                <li key={text} className="p-0">
+                                    <button
+                                        className="p-5 border border-transparent hover:border-[#D9D9D9] hover:bg-[#F2E9E9] w-full text-left rounded-lg hover:shadow-lg"
+                                        onClick={() =>
+                                            navigate(
+                                                `/admin/${text
+                                                    .toLowerCase()
+                                                    .replace(" ", "-")}`
+                                            )
+                                        }
+                                    >
+                                        {text}
+                                    </button>
+                                </li>
+                            )
+                        )}
+                    </ul>
+                </div>
+                
             </div>
+            <button className="p-5 border border-transparent hover:border-[#D9D9D9] hover:bg-[#C90000] hover:text-[#F2E9E9] w-full text-left rounded-lg hover:shadow-lg" 
+             onClick={() => {
+                logout();  // Call logout function
+                navigate("/admin");  // Redirect to login page
+            }}>
+                Logout
+            </button>
         </div>
     );
 }
